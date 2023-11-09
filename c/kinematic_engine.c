ca6e76c26e5e55f4fc008d5e09f790097478b73d
lukecartier <lcartier@calpoly.edu>
2023-11-09 13:23:30 -0800

    Fixed File Position and Makefile

--- c/kinematic_engine.c
+++ c/kinematic_engine.c
@@ -7,7 +7,7 @@ double law_of_cosines(double a, double b, double c) {
 
 
 int to_deg(double radians) {
-    return (int) (radians * RAD_TO_DEG)
+    return (int) (radians * RAD_TO_DEG);
 }
 
 
@@ -21,7 +21,7 @@ bool kinematic_engine(float x_pos, float y_pos, float z_pos,
 
         magnitude = sqrt(x_pos * x_pos + y_pos * y_pos);
         if (magnitude * .99 > S_E_LENGTH + E_W_LENGTH) {//check if in range
-            return false
+            return false;
         }
 
         theta_1 = law_of_cosines(S_E_LENGTH, magnitude, E_W_LENGTH);
@@ -29,6 +29,6 @@ bool kinematic_engine(float x_pos, float y_pos, float z_pos,
 
         *shoulder_angle = 180 - to_deg(theta_1 + atan(y_pos / x_pos));
         *elbow_angle = to_deg(PI -  theta_1 - theta_2);
-        *wrist_angle = 280 - to_deg(3 * PI / 2 - *shoulder_angle - *elbow_angle)
+        *wrist_angle = 280 - to_deg(3 * PI / 2 - *shoulder_angle - *elbow_angle);
         return true;
     }
