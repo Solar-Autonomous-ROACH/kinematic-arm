71da5e75aa1d1fea113cf8e1a70aca7c0624f25d
Christian Honein <61818445+chonein@users.noreply.github.com>
2023-11-16 13:25:17 -0800

    fixed kin engine bugs

--- c/main.c
+++ c/main.c
@@ -4,9 +4,9 @@
 
 #include "arm.h"
 #include "isr.h"
+#include "kinematic_engine.h"
 #include "led.h"
 #include "mmio.h"
-#include "kinematic_engine.h"
 #include <signal.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -31,13 +31,13 @@ int main() {
   set_led_status();
   set_brightness(100, 100, 000);
   speed1 = 0;
-  int16_t calc_base_angle, calc_elbow_angle, calc_wrist_angle;
+  int16_t calc_base_angle, calc_elbow_angle, calc_wrist_angle, calc_turn_angle;
   float x_in, y_in, z_in;
   while (1) {
     // scanf("%d", &speed1);
-    if (scanf("%f %f %f\n", &x_in, &y_in,
-              &z_in) > 0) {
-      kinematic_engine(x_in, y_in, z_in, &calc_base_angle, &calc_elbow_angle, &calc_wrist_angle);
+    if (scanf("%f %f %f\n", &x_in, &y_in, &z_in) > 0) {
+      kinematic_engine(x_in, y_in, z_in, &calc_base_angle, &calc_elbow_angle,
+                       &calc_wrist_angle, &calc_turn_angle);
       validate_angle_set(calc_base_angle, calc_elbow_angle, calc_wrist_angle);
       printf("HERE");
     }
