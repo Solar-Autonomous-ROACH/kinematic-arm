d19d9776349843ccce0361129e23c5a34dc8cb84
lukecartier <lcartier@calpoly.edu>
2024-01-18 13:41:08 -0800

    Kine Output Struct in Header

--- c/kinematic_engine.h
+++ c/kinematic_engine.h
@@ -32,10 +32,19 @@
 
 #define SMALL_DOUBLE 0.000001
 
+typedef struct {
+  int16_t extra_distance; //Extra distance to get within arm range
+  int16_t turn_angle; //Angle to turn to align for arm
+  int16_t shoulder_angle; //If both 0, fill these out
+  int16_t elbow_angle;
+  int16_t wrist_angle;
+  bool error; // Any other error that causes the engine to have to exit, probably bounds
+} kinematic_output_t
+
 double law_of_cosines(double a, double b, double c);
 bool kinematic_engine(float x_pos, float y_pos, float z_pos,
                       int16_t *shoulder_angle, int16_t *elbow_angle,
-                      int16_t *wrist, int16_t *turn_angle);
+                      int16_t *wrist_angle, int16_t *turn_angle);
 int to_deg(double radians);
 
 #endif
